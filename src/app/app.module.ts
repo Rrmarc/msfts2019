cc78cd574edaedc4ea539eb23550a1d6fb4f93e1
Rashell Marcelino <rashellmarcelino@Rashells-MacBook-Air.local>
2019-05-14 15:20:20 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
